be79d821485a26262a68846ae40120013f657181
luana rodrigues <luana.borges1@ufu.br>
2024-08-28 22:42:59 -0300

    lab01

--- LAB01 - Procedural/ex03/src/principal.java
+++ LAB01 - Procedural/ex03/src/principal.java
@@ -88,7 +88,7 @@ public class principal {
         Scanner sc = new Scanner(System.in);
         int[] vet = new int[5];
         int soma = 0, maior, menor;
-        double media = 0;
+        double media;
 
         System.out.println("<< 5 valores >>");
         for (int i = 0; i < 5; i++){
@@ -116,7 +116,7 @@ public class principal {
         Scanner sc = new Scanner(System.in);
         int[] vet = new int[5];
         int soma = 0, maior, posmaior = 0, posmenor = 0, menor;
-        double media = 0;
+        double media;
 
         System.out.println("<< 5 valores >>");
         for (int i = 0; i < 5; i++){
@@ -215,18 +215,247 @@ public class principal {
 
     }
 
+    public static void ex10(){
+        Scanner sc = new Scanner(System.in);
+        int[] vetA = new int[3];
+        int[] vetB = new int[3];
+        int[] vetC = new int[3];
+        System.out.println("<< Subtração de vetores >>");
+        for (int i = 0; i < 3; i++){
+            System.out.printf("Digite o valor %d de A: ",i+1);
+            vetA[i] = sc.nextInt();
+        }
+        System.out.print("\n");
+        for (int i = 0; i < 3; i++){
+            System.out.printf("Digite o valor %d de B: ",i+1);
+            vetB[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 3; i++){
+            vetC[i] = vetA[i] - vetB[i];
+        }
 
+        System.out.printf("\nO vetor C, definido como C = A - B é (%d,%d,%d)",vetC[0],vetC[1],vetC[2]);
+    }
+
+    public static void ex11(){
+        Scanner sc = new Scanner(System.in);
+        int[] vet = new int[5];
+        int[] vet1 = new int[5];
+        int[] vet2 = new int[5];
+        int aux1 = 0, aux2 = 0;
+
+        System.out.println("<< Pares e Ímpares >>");
+        for (int i = 0; i < 5; i++){
+            System.out.printf("Digite o valor %d: ",i+1);
+            vet[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 5; i++){
+            if(vet[i]%2 == 0) {
+                vet2[aux2] = vet[i];
+                aux2++;
+            }else {
+                vet1[aux1] = vet[i];
+                aux1++;
+            }
+        }
+
+        System.out.print("\nÍmpares: ");
+        for (int i = 0; i < aux1; i++){
+            System.out.printf("%d",vet1[i]);
+        }
+        System.out.print("\nPares: ");
+        for (int i = 0; i < aux2-1; i++){
+            System.out.printf("%d,",vet2[i]);
+        }
+        System.out.printf("%d", vet2[aux2-1]);
+    }
+
+    public static void ex12(){
+        Scanner sc = new Scanner(System.in);
+        int[] vet = new int[4];
+        System.out.print("<< Probabilidades >>");
+        System.out.println("Digite a quantidade de bolinhas");
+        int verde, azul, amarela,vermelha;
+        System.out.print("Verde: ");
+        verde = sc.nextInt();
+        vet[0] = verde;
+        System.out.print("Azul: ");
+        azul = sc.nextInt();
+        vet[1] = azul;
+        System.out.print("Amarela: ");
+        amarela = sc.nextInt();
+        vet[2] = amarela;
+        System.out.print("Vermelha: ");
+        vermelha = sc.nextInt();
+        vet[3] = vermelha;
+        int soma = verde + azul + amarela + vermelha;
+        double[] vetp = new double[4];
+        double maior, posmaior = 0;
+        System.out.println("\nProbabilidades");
+        for(int i = 0; i < 4; i++){
+            vetp[i] = 100*vet[i]/(soma*1.0);
+        }
+        maior = vetp[0];
+        for(int i = 0; i < 4; i++){
+            if(vetp[i] > maior) {
+                maior = vetp[i];
+                posmaior = i;
+            }
+        }
+        System.out.print("Verde: ");
+        if(posmaior == 0)  System.out.printf("%.1f%c <<maior probabilidade",vetp[0],'%');
+        else System.out.printf("%.1f%c ",vetp[0],'%');
+        System.out.print("\nAzul: ");
+        if(posmaior == 1)  System.out.printf("%.1f%c <<maior probabilidade",vetp[1], '%');
+        else System.out.printf("%.1f%c ",vetp[1],'%');
+        System.out.print("\nAmarela: ");
+        if(posmaior == 2)  System.out.printf("%.1f%c <<maior probabilidade",vetp[2],'%');
+        else System.out.printf("%.1f%c ",vetp[2],'%');
+        System.out.print("\nVermelha: ");
+        if(posmaior == 3)  System.out.printf("%.1f%c <<maior probabilidade",vetp[3],'%');
+        else System.out.printf("%.1f%c ",vetp[3],'%');
+
+    }
+
+    public static void ex13(){
+        Scanner sc = new Scanner(System.in);
+        int[] vet = new int[5];
+        System.out.println("<< Zerando negativos >>");
+        for (int i = 0; i < 5; i++){
+            System.out.printf("Entre com o número %d: ",i+1);
+            vet[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 5; i++){
+            if(vet[i] < 0) vet[i] = 0;
+        }
+        System.out.print("Zerando os negativos, obtém-se: ");
+        for (int i = 0; i < 5; i++){
+            System.out.printf("%d ", vet[i]);
+        }
+
+    }
+
+    public static void ex14(){
+        Scanner sc = new Scanner(System.in);
+        int tam;
+        System.out.println("<< Universidade X>>");
+        System.out.print("Quantos alunos serão cadastrados: ");
+        tam = sc.nextInt();
+        if(tam > 1000){
+            System.out.print("Erro! O número máximo de alunos permitido é 1000.");
+        }else{
+            int[] alunos = new int[tam];
+            char[] classeSocial= new char[tam];
+            double[] cRA= new double[tam];
+
+            for(int i = 0; i < tam; i++){
+                System.out.print("\nEntre com o número do aluno: ");
+                alunos[i]= sc.nextInt();
+                System.out.print("Entre com a classe social do aluno: ");
+                classeSocial[i]= sc.next().charAt(0);
+                System.out.print("Entre com o número do aluno: ");
+                cRA[i]= sc.nextDouble();
+
+            }
+            System.out.println("\n==== Alunos Cadastrados ==== ");
+            for(int i = 0; i < tam; i++){
+                System.out.printf("Número: %d Classe Social: %c CRA: %.2f\n", alunos[i],classeSocial[i],cRA[i]);
+            }
+
+        }
+
+
+    }
+    
+    public static void ex15(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("<< Valores iguais >>");
+        int[] vet = new int[8];
+        int[] repete = new int[8];
+        int aux = 0;
+        for(int i = 0; i < 8; i++){
+            System.out.printf("Entre com o número %d: ",i+1);
+            vet[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 8; j++) {
+                if (vet[i] == vet[j]) {
+                    int cont = 0;
+                    for (int k = 0; k < 8; k++) {
+                        if(repete[k]==vet[i]){
+                        cont = 1;
+                        break;
+                        }
+                    }
+                    if (cont == 0) {
+                        repete[aux] = vet[i];
+                        aux++;
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.print("\nValores repetidos: ");
+        for (int i = 0; i < aux-1; i++) {
+            System.out.printf("%d, ", repete[i]);
+        }
+        System.out.printf("%d ", repete[aux-1]);
+    }
+
+    public static void ex16(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("<< Valores iguais >>");
+        int[] vet = new int[8];
+        int[] repete = new int[8];
+        for(int i = 0; i < 8; i++){
+            System.out.printf("Entre com o número %d: ",i+1);
+            vet[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 8; i++) {
+            repete[i] = -1;
+        }
+        for (int i = 0; i < 8; i++) {
+            int count = 1;
+            for (int j = i + 1; j < 8; j++) {
+                if (vet[i] == vet[j]) {
+                    count++;
+                    repete[j] = 0;
+                }
+            }
+            if (repete[i] != 0) {
+                repete[i] = count;
+            }
+        }
+        System.out.println("\nValores repetidos:");
+        int temIgual = 0;
+        for (int i = 0; i < 8; i++) {
+            if (repete[i] > 1) {
+                System.out.printf("%d aparece %d vezes \n",vet[i], repete[i]);
+                temIgual = 1;
+            }
+        }
+        if (temIgual == 0) {
+            System.out.println("Não existem valores iguais no vetor.");
+        }
+    }
 
     public static void main(String[] args) {
-        //ex01();
-        //ex02();
-        //ex03();
-        //ex04();
-        //ex05();
-        //ex06();
-        //ex07();
-        //ex08();
-        //ex09();
+        ex01();
+        ex02();
+        ex03();
+        ex04();
+        ex05();
+        ex06();
+        ex07();
+        ex08();
+        ex09();
+        ex10();
+        ex11();
+        ex12();
+        ex13();
+        ex14();
+        ex15();
+        ex16();
 
     }
 
