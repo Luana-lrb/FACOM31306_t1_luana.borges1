38356c9a46f60bfe8c4a5f700f7e3460d5811a62
luana rodrigues <luana.borges1@ufu.br>
2024-11-02 18:17:29 -0300

    Lab05

---
+++ LAB05 - Polymorphism/ex03/ex03/src/EstudanteGraduacao.java
@@ -0,0 +1,37 @@
+public class EstudanteGraduacao extends Estudante {
+    private String tituloTCC;
+
+    public EstudanteGraduacao(String nome, String endereco, String tituloTCC) {
+        super(nome, endereco);
+        this.tituloTCC = tituloTCC;
+    }
+
+    public void print() {
+        super.print();
+        System.out.println("EstudanteGraduacao{" +
+                "tituloTCC='" + tituloTCC + '\'' +
+                '}');
+    }
+
+    public void print(boolean linhaUnica) {
+        super.print();
+        if(linhaUnica){
+            System.out.println("EstudanteGraduacao{" +
+                    "tituloTCC='" + tituloTCC + '\'' +
+                    '}');
+        } else {
+            System.out.println("EstudanteGraduacao{" +
+                    "tituloTCC='" + tituloTCC + '\'' +
+                    '}');
+        }
+
+    }
+
+    public String getTituloTCC() {
+        return tituloTCC;
+    }
+
+    public void setTituloTCC(String tituloTCC) {
+        this.tituloTCC = tituloTCC;
+    }
+}
---
+++ LAB05 - Polymorphism/ex03/ex03/src/Universidade.java
@@ -0,0 +1,6 @@
+public class Universidade {
+    private String nome;
+    private String fundacao;
+    private Estudante[] estudantes = new Estudante[100];
+
+}
