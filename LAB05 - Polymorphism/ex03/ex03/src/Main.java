8758e6330690e42ef5b40491358e5c1542d05118
luana rodrigues <luana.borges1@ufu.br>
2024-11-08 21:04:43 -0300

    Final Lab05

--- LAB05 - Polymorphism/ex03/ex03/src/Main.java
+++ LAB05 - Polymorphism/ex03/ex03/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Estudante[] es = new Estudante[8];
+        es[0] = new Estudante("Carlos","Rua 1o de Abril");
+        es[1] = new EstudantePosGrad("Ailton","Rua 31 de fevereiro","Engenharia", "Banco de dados");
+        es[2] = new EstudanteMestrado("Carla","Rua Primavera","Medicina",
+                "Vacinas de RNA", "academico","Anticorpos");
+        es[3] = new EstudanteDoutorado("Bruno","Rua Inverno",
+                "Tecnologia","Processamento de Imagens","Carros Inteligentes");
+        es[4]= new EstudanteGraduacao("Maria", "Rua Maio", "Java is funny");
+        es[5] = new EstudanteMestrado("Carla","Rua Primavera","Medicina",
+                "Vacinas de RNA", "Anticorpos","academico");
+        es[6] = new EstudanteDoutorado("Bruno","Rua Inverno",
+                "Tecnologia","Processamento de Imagens","Carros Inteligentes");
+
+
+        Universidade UFU = new Universidade("UFU","34/05/1930",es);
+
+        EstudantePosGrad [] posgrads = new EstudantePosGrad[100];
+
+        UFU.contaEstudantes();
+        UFU.copiaPosGraduandos(posgrads);
+        UFU.imprimeTudo();
     }
 }
--- LAB05 - Polymorphism/ex03/ex03/src/Universidade.java
+++ LAB05 - Polymorphism/ex03/ex03/src/Universidade.java
@@ -3,4 +3,47 @@ public class Universidade {
     private String fundacao;
     private Estudante[] estudantes = new Estudante[100];
 
+    public Universidade(String nome, String fundacao, Estudante[] estudantes) {
+        this.nome = nome;
+        this.fundacao = fundacao;
+        this.estudantes = estudantes;
+    }
+
+    public void contaEstudantes(){
+        int contGraduacao = 0, contDoutorado = 0, contMestrado = 0;
+        for(Estudante iteracao: estudantes ){
+            if(iteracao != null) {
+                if (iteracao instanceof EstudanteGraduacao) contGraduacao++;
+                if (iteracao instanceof EstudanteMestrado) contMestrado++;
+                if (iteracao instanceof EstudanteDoutorado) contDoutorado++;
+            }
+        }
+        System.out.println("A quantidade de graduandos é: " + contGraduacao );
+        System.out.println("A quantidade de graduandos é: " + contMestrado );
+        System.out.println("A quantidade de graduandos é: " + contDoutorado );
+
+    }
+
+    public void imprimeTudo(){
+        for(Estudante iteracao: estudantes ){
+            if(iteracao != null)
+            iteracao.print();
+        }
+    }
+
+    public void copiaPosGraduandos(EstudantePosGrad[] posGraduandos) {
+        int i = 0;
+        for (Estudante iteracao : estudantes) {
+            if(iteracao != null) {
+                if (iteracao instanceof EstudantePosGrad) {
+                    posGraduandos[i] = (EstudantePosGrad) iteracao;
+                    i++;
+                }
+            }
+        }
+        for(Estudante iteracao:  posGraduandos){
+            if(iteracao != null) iteracao.print();
+        }
+
+    }
 }
